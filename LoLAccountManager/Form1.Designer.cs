580985a0b2a786f749f5284badab99413a880129
SpiffyAF <couchstub@gmail.com>
2022-10-26 19:34:56 -0400

    UI updates

--- LoLAccountManager/Form1.Designer.cs
+++ LoLAccountManager/Form1.Designer.cs
@@ -33,18 +33,18 @@ namespace LoLAccountManager
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
             this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.toolbarPanel = new Guna.UI2.WinForms.Guna2Panel();
+            this.switchViewPanel = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2DragControl1 = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.exit_Button = new Guna.UI2.WinForms.Guna2CircleButton();
             this.loadClientButton = new Guna.UI2.WinForms.Guna2Button();
             this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
             this.dashButton = new Guna.UI2.WinForms.Guna2Button();
-            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
             this.guna2CirclePictureBox1 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
-            this.toolbarPanel = new Guna.UI2.WinForms.Guna2Panel();
-            this.exit_Button = new Guna.UI2.WinForms.Guna2CircleButton();
-            this.switchViewPanel = new Guna.UI2.WinForms.Guna2Panel();
-            this.guna2DragControl1 = new Guna.UI2.WinForms.Guna2DragControl(this.components);
             this.guna2Panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).BeginInit();
             this.toolbarPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // guna2Elipse1
@@ -64,6 +64,61 @@ namespace LoLAccountManager
             this.guna2Panel1.Name = "guna2Panel1";
             this.guna2Panel1.Size = new System.Drawing.Size(200, 450);
             this.guna2Panel1.TabIndex = 0;
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Franklin Gothic Demi", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel1.ForeColor = System.Drawing.Color.White;
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(59, 29);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(131, 23);
+            this.guna2HtmlLabel1.TabIndex = 1;
+            this.guna2HtmlLabel1.Text = "Account Manager";
+            // 
+            // toolbarPanel
+            // 
+            this.toolbarPanel.Controls.Add(this.exit_Button);
+            this.toolbarPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.toolbarPanel.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(14)))), ((int)(((byte)(24)))));
+            this.toolbarPanel.Location = new System.Drawing.Point(200, 0);
+            this.toolbarPanel.Name = "toolbarPanel";
+            this.toolbarPanel.Size = new System.Drawing.Size(600, 28);
+            this.toolbarPanel.TabIndex = 1;
+            // 
+            // switchViewPanel
+            // 
+            this.switchViewPanel.BackColor = System.Drawing.Color.White;
+            this.switchViewPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.switchViewPanel.Location = new System.Drawing.Point(200, 28);
+            this.switchViewPanel.Name = "switchViewPanel";
+            this.switchViewPanel.Size = new System.Drawing.Size(600, 422);
+            this.switchViewPanel.TabIndex = 2;
+            // 
+            // guna2DragControl1
+            // 
+            this.guna2DragControl1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2DragControl1.TargetControl = this.toolbarPanel;
+            this.guna2DragControl1.UseTransparentDrag = true;
+            // 
+            // exit_Button
+            // 
+            this.exit_Button.BackColor = System.Drawing.Color.Transparent;
+            this.exit_Button.BorderColor = System.Drawing.Color.Transparent;
+            this.exit_Button.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.exit_Button.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.exit_Button.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.exit_Button.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.exit_Button.FillColor = System.Drawing.Color.Transparent;
+            this.exit_Button.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.exit_Button.ForeColor = System.Drawing.Color.Transparent;
+            this.exit_Button.Image = ((System.Drawing.Image)(resources.GetObject("exit_Button.Image")));
+            this.exit_Button.Location = new System.Drawing.Point(561, 3);
+            this.exit_Button.Name = "exit_Button";
+            this.exit_Button.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.exit_Button.Size = new System.Drawing.Size(27, 22);
+            this.exit_Button.TabIndex = 0;
+            this.exit_Button.Click += new System.EventHandler(this.exit_Button_Click);
             // 
             // loadClientButton
             // 
@@ -133,24 +188,13 @@ namespace LoLAccountManager
             this.dashButton.TabIndex = 2;
             this.dashButton.Text = "Dashboard";
             // 
-            // guna2HtmlLabel1
-            // 
-            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
-            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Franklin Gothic Demi", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.guna2HtmlLabel1.ForeColor = System.Drawing.Color.White;
-            this.guna2HtmlLabel1.Location = new System.Drawing.Point(63, 29);
-            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
-            this.guna2HtmlLabel1.Size = new System.Drawing.Size(131, 23);
-            this.guna2HtmlLabel1.TabIndex = 1;
-            this.guna2HtmlLabel1.Text = "Account Manager";
-            // 
             // guna2CirclePictureBox1
             // 
             this.guna2CirclePictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.guna2CirclePictureBox1.FillColor = System.Drawing.Color.Transparent;
             this.guna2CirclePictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("guna2CirclePictureBox1.Image")));
             this.guna2CirclePictureBox1.ImageRotate = 0F;
-            this.guna2CirclePictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.guna2CirclePictureBox1.Location = new System.Drawing.Point(8, 12);
             this.guna2CirclePictureBox1.Name = "guna2CirclePictureBox1";
             this.guna2CirclePictureBox1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
             this.guna2CirclePictureBox1.Size = new System.Drawing.Size(54, 57);
@@ -158,50 +202,6 @@ namespace LoLAccountManager
             this.guna2CirclePictureBox1.TabIndex = 0;
             this.guna2CirclePictureBox1.TabStop = false;
             this.guna2CirclePictureBox1.UseTransparentBackground = true;
-            // 
-            // toolbarPanel
-            // 
-            this.toolbarPanel.Controls.Add(this.exit_Button);
-            this.toolbarPanel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.toolbarPanel.FillColor = System.Drawing.Color.White;
-            this.toolbarPanel.Location = new System.Drawing.Point(200, 0);
-            this.toolbarPanel.Name = "toolbarPanel";
-            this.toolbarPanel.Size = new System.Drawing.Size(600, 28);
-            this.toolbarPanel.TabIndex = 1;
-            // 
-            // exit_Button
-            // 
-            this.exit_Button.BackColor = System.Drawing.Color.Transparent;
-            this.exit_Button.BorderColor = System.Drawing.Color.Transparent;
-            this.exit_Button.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.exit_Button.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.exit_Button.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.exit_Button.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.exit_Button.FillColor = System.Drawing.Color.Transparent;
-            this.exit_Button.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.exit_Button.ForeColor = System.Drawing.Color.Transparent;
-            this.exit_Button.Image = ((System.Drawing.Image)(resources.GetObject("exit_Button.Image")));
-            this.exit_Button.Location = new System.Drawing.Point(561, 3);
-            this.exit_Button.Name = "exit_Button";
-            this.exit_Button.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
-            this.exit_Button.Size = new System.Drawing.Size(27, 22);
-            this.exit_Button.TabIndex = 0;
-            this.exit_Button.Click += new System.EventHandler(this.exit_Button_Click);
-            // 
-            // switchViewPanel
-            // 
-            this.switchViewPanel.BackColor = System.Drawing.Color.White;
-            this.switchViewPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.switchViewPanel.Location = new System.Drawing.Point(200, 28);
-            this.switchViewPanel.Name = "switchViewPanel";
-            this.switchViewPanel.Size = new System.Drawing.Size(600, 422);
-            this.switchViewPanel.TabIndex = 2;
-            // 
-            // guna2DragControl1
-            // 
-            this.guna2DragControl1.DockIndicatorTransparencyValue = 0.6D;
-            this.guna2DragControl1.TargetControl = this.toolbarPanel;
-            this.guna2DragControl1.UseTransparentDrag = true;
             // 
             // Form1
             // 
@@ -218,8 +218,8 @@ namespace LoLAccountManager
             this.Load += new System.EventHandler(this.Form1_Load);
             this.guna2Panel1.ResumeLayout(false);
             this.guna2Panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).EndInit();
             this.toolbarPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
