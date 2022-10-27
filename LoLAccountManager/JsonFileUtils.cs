3efe18d4118eb15874a552d92364a7f9d143ef59
SpiffyAF <couchstub@gmail.com>
2022-10-27 14:57:42 -0400

    added JsonUtils for serializing accounts

---
+++ LoLAccountManager/JsonFileUtils.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace LoLAccountManager
+{
+    public static class JsonFileUtils
+    {
+        public static void JsonWrite(object obj, string fileName)
+        {
+            var jsonString = JsonConvert.SerializeObject(obj, Formatting.Indented);
+            File.WriteAllText(fileName, jsonString);
+        }
+
+        public static List<Account> JsonRead(List<Account> obj, string fileName)
+        {
+            string json = File.ReadAllText(fileName);
+            obj = JsonConvert.DeserializeObject<List<Account>>(json);
+            return obj;
+        }
+    }
+}
